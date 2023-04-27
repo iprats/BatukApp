c50402378b935d18d96b147b0dc235a71fe6569b
iprats <50885891+iprats@users.noreply.github.com>
2023-04-27 19:53:17 +0200

    Maquejar Codi

--- batukapp/app/Http/Controllers/GoogleController.php
+++ batukapp/app/Http/Controllers/GoogleController.php
@@ -21,30 +21,17 @@ class GoogleController extends Controller
     public static function googleCallback()
     {
         $user_google = Socialite::driver('google')->stateless()->user();
-
-        //dd($user_google);
-
-    
         $user = ApiController::callApi("/users", true, "POST", $user_google);
 
-        //dd($user);
-
         session_start();
-
         session(["google_id" => $user->google_id]);
 
-        //dd($_SESSION);
-        
-    
         return redirect("/dashboard");
     }
 
     public static function logout(Request $request)
     {
-        //dd("has sortit");
-
         $request->session()->invalidate();
-
         $request->session()->regenerateToken();
 
         if(isset($_COOKIE["google_id"]))
